2a19054a403b3543f4c0d6960ca5e2c75562d6c8
Ian Cowan <ian@cowanemail.com>
2021-02-23 21:24:12 -0500

    Remove num moves

--- CubeA1/TestCube.java
+++
@@ -1,8 +0,0 @@
-public class TestCube {
-    public static void main(String[] args) {
-        Cube cube = new Cube();
-        cube.front(1);
-        cube.back(3);
-        cube.print();
-    }
-}
